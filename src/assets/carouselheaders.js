aad458b87e87b558b9a7704bd50ab8f9433db452
Grady Ku <gkupdx@gmail.com>
2022-02-22 12:50:20 -0800

    Add new placeholder API data for carousel headers, movies, and TV shows

---
+++ src/assets/carouselheaders.js
@@ -0,0 +1,42 @@
+export const carouselHeaders = [
+    {
+        id: '1',
+        header: 'Watch It Again'
+    },
+    {
+        id: '2',
+        header: 'Trending Now'
+    },
+    {
+        id: '3',
+        header: 'New Releases'
+    },
+    {
+        id: '4',
+        header: 'Only on Netflix'
+    },
+    {
+        id: '5',
+        header: 'My List'
+    },
+    {
+        id: '6',
+        header: 'Continue Watching for [User]'
+    },
+    {
+        id: '7',
+        header: 'Top 10 in the U.S. Today'
+    },
+    {
+        id: '8',
+        header: 'Popular on Netflix'
+    },
+    {
+        id: '9',
+        header: 'Critically Acclaimed Films'
+    },
+    {
+        id: '10',
+        header: 'Blockbuster Movies'
+    }
+]
