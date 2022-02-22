846159bdf7184b02363b32c51675c6bceae20cde
Grady Ku <gkupdx@gmail.com>
2022-02-21 16:06:36 -0800

    Import new components, Subsection and Footer. Add temp array of subsection content. Render Subsection and Footer components.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@
 
 import Nav from './components/Nav';
 import Banner from './components/Banner';
+import Subsection from './components/Subsection';
+import Footer from './components/Footer';
 
 // Routes from React Router
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
+  const subsectionContent = [
+    {
+      header: 'Enjoy on your TV.',
+      body: 'Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.'
+    },
+    {
+      header: 'Download your shows to watch offline.',
+      body: 'Save your favorites easily and always have something to watch.'
+    },
+    {
+      header: 'Watch everywhere.',
+      body: 'Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.'
+    },
+    {
+      header: 'Create profiles for kids.',
+      body: 'Send kids on adventures with their favorite characters in a space made just for them--free with your membership.'
+    }
+  ];
+
   return (
     <div className="container">
       <Routes>
@@ -14,8 +35,13 @@ function App() {
           <>
             <Nav />
             <Banner />
+            <Subsection header={subsectionContent[0].header} body={subsectionContent[0].body} />
+            <Subsection header={subsectionContent[1].header} body={subsectionContent[1].body} />
+            <Subsection header={subsectionContent[2].header} body={subsectionContent[2].body} />
+            <Subsection header={subsectionContent[3].header} body={subsectionContent[3].body} />
+            <Footer />
           </>
-        } />
+          } />
       </Routes>
     </div>
   );
