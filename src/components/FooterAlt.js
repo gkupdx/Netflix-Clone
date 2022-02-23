4fa7f6702e4093d74a6ba855e2f4753b13176c12
Grady Ku <gkupdx@gmail.com>
2022-02-22 16:20:22 -0800

    Add new FooterAlt.js component

---
+++ src/components/FooterAlt.js
@@ -0,0 +1,28 @@
+//// FooterAlt.js - component for alternate footer placed at bottom of Sign In page
+
+const FooterAlt = ({ toggleLanguages, toggle, globeIcon, caretIcon }) => {
+    return (
+        <footer className='footerAlt'>
+            <div className='footerWrapper'>
+                <p>Questions? Call <a href='tel:1-844-505-2993'>1-844-505-2993</a></p>
+
+
+                <div className='linksWrapperAlt'>
+                    <a href='#faq'>FAQ</a>
+                    <a href='#help'>Help Center</a>
+                    <a href='#tos'>Terms of Use</a>
+                    <a href='#privacy'>Privacy</a>
+                    <a href='#cookies'>Cookie Preferences</a>
+                    <a href='#ci'>Corporate Information</a>
+                </div>
+
+                <div>
+                    {/* {toggle ? <div className='languagePopUp'><p>English</p><p>Espanol</p></div> : ''} */}
+                    <button onClick={toggleLanguages}>{globeIcon} English {caretIcon}</button>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default FooterAlt;
