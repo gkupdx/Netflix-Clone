e0b7e1cddce65accf6cc777ffe29d60c3572e193
Grady Ku <gkupdx@gmail.com>
2022-02-22 16:19:10 -0800

    Move shared code logic/imports/objects to Homepage container component and pass into Nav as props

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,8 @@
 //// Nav.js - component for navbar
 
-import { useState } from 'react';
-
-import { IoGlobe } from 'react-icons/io5';
-import { AiFillCaretDown } from 'react-icons/ai';
 import logo from '../assets/netflix_text_logo.png';
 
-const Nav = () => {
-    const [show, setShow] = useState(false);
-
-    // Globe icon styling
-    const globeIconStyle = {
-        color: "#FFF"
-    }
-
-    // Toggle for languages button
-    const showLanguages = () => {
-        setShow(!show);
-    }
+const Nav = ({ toggleLanguages, toggle, globeIcon, caretIcon }) => {
 
     return (
         <nav className='nav'>
@@ -25,8 +10,8 @@ const Nav = () => {
 
             <ul>
                 <li>
-                    <button className='languageBtn' onClick={showLanguages}><IoGlobe style={globeIconStyle}/> English <AiFillCaretDown /></button>
-                    {show ? <div className='languageDropDown'><p>English</p><p>Espanol</p></div> : ''}
+                    <button className='languageBtn' onClick={toggleLanguages}>{globeIcon} English {caretIcon}</button>
+                    {toggle ? <div className='languageDropDown'><p>English</p><p>Espanol</p></div> : ''}
                     <a href='/login' className='signIn'>Sign In</a>
                 </li>
             </ul>
