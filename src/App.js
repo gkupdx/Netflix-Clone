fd2cd5e859c8d718172d2c7534a6aba777e7845e
Grady Ku <gkupdx@gmail.com>
2022-02-28 16:59:09 -0800

    Import & render newly added components; add temporary inline styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 //// App.js
 
 import Homepage from './components/Homepage';
-import SignUppage from './components/SignUppage';
+import Registration from './components/Registration';
+import RegForm from './components/RegForm';
+import SignUp from './components/SignUp';
+import PlanForm from './components/PlanForm';
+// import Payment from './components/Payment';
+
 import Loginpage from './components/Loginpage';
+import FooterAlt from './components/FooterAlt';
 
 import { useState } from 'react';
+import { IoGlobe } from 'react-icons/io5';
+import { AiFillCaretDown } from 'react-icons/ai';
+
+import logoImgSrc from './assets/netflix_text_logo.png'
 
 // Routes from React Router
 import { Routes, Route } from 'react-router-dom';
@@ -17,31 +27,76 @@ function App() {
     setToggle(!toggle)
   }
 
+  // different footer theme depending on page
+  const footerTheme = "light";
+
+  // Globe icon styling 
+  const globeIconStyle = {
+    color: "#000"
+  }
+
+  // Caret down icon styling 
+  const caretIconStyle = {
+    color: "#000"
+  }
+
   return (
     <div className="container">
       <Routes>
         {/* HOMEPAGE / LANDING PAGE */}
         <Route path='/' element={
           <>
-            <Homepage toggleLanguages={toggleLanguages} toggle={toggle}/>
+            <Homepage toggleLanguages={toggleLanguages} toggle={toggle} />
           </>
-          } 
+        }
         />
 
+        {/* INITIAL REGISTRATION PAGE */}
         <Route path='/signup/registration' element={
           <>
-            <SignUppage toggleLanguages={toggleLanguages} toggle={toggle}/>
+            <Registration logo={logoImgSrc} />
+            <FooterAlt toggleLanguages={toggleLanguages} toggle={toggle} globeIcon={<IoGlobe style={globeIconStyle} />} caretIcon={<AiFillCaretDown style={caretIconStyle} />} theme={footerTheme} />
           </>
-        }>
+        } />
 
-        </Route>
+        {/* STEP 1 of 3 REGISTRATION */}
+        <Route path='/signup/regform' element={
+          <>
+            <RegForm logo={logoImgSrc} />
+            <FooterAlt toggleLanguages={toggleLanguages} toggle={toggle} globeIcon={<IoGlobe style={globeIconStyle} />} caretIcon={<AiFillCaretDown style={caretIconStyle} />} theme={footerTheme} />
+          </>
+        } />
+
+        {/* STEP 2 of 3 REGISTRATION */}
+        <Route path='/signup' element={
+          <>
+            <SignUp logo={logoImgSrc} />
+            <FooterAlt toggleLanguages={toggleLanguages} toggle={toggle} globeIcon={<IoGlobe style={globeIconStyle} />} caretIcon={<AiFillCaretDown style={caretIconStyle} />} theme={footerTheme} />
+          </>
+        } />
+
+        {/* STEP 2 continued... */}
+        <Route path='/signup/planform' element={
+          <>
+            <PlanForm logo={logoImgSrc} />
+            <FooterAlt toggleLanguages={toggleLanguages} toggle={toggle} globeIcon={<IoGlobe style={globeIconStyle} />} caretIcon={<AiFillCaretDown style={caretIconStyle} />} theme={footerTheme} />
+          </>
+        } />
+
+        {/* STEP 3 of 3 REGISTRATION */}
+        {/* <Route path='/signup/payment' element={
+          <>
+            <Payment logo={logoImgSrc} />
+            <FooterAlt toggleLanguages={toggleLanguages} toggle={toggle} globeIcon={<IoGlobe style={globeIconStyle} />} caretIcon={<AiFillCaretDown style={caretIconStyle} />} theme={footerTheme} />
+          </>
+        } /> */}
 
         {/* LOGIN PAGE */}
         <Route path='/login' element={
           <>
-            <Loginpage toggleLanguages={toggleLanguages} toggle={toggle}/>
+            <Loginpage toggleLanguages={toggleLanguages} toggle={toggle} />
           </>
-          }
+        }
         />
       </Routes>
     </div>
