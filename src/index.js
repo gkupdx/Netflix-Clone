7ffb6754cb78040cd62339ef464d134f2b86c07e
Grady Ku <gkupdx@gmail.com>
2022-03-03 14:11:08 -0800

    Import newly added CSS files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './registration.css';
+import './login.css';
 import App from './App';
 
 // React Router
