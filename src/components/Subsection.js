be1887d2a2e4e6f6057931e46e7e74a2d7952a4e
Grady Ku <gkupdx@gmail.com>
2022-03-07 14:07:49 -0800

    Add logic to switch JSX element positions depending on browser width

--- src/components/Subsection.js
+++ src/components/Subsection.js
@@ -1,24 +1,54 @@
 //// Subsection.js - component for the 4 subsections below Banner.js
 
-const Subsection = ({ header, body, imgSrc }) => {
+import { useState, useEffect } from 'react';
 
-    // Image styling
-    const imgStyle = {
-        width: '75%',
-        height: '100%'
+const Subsection = ({ header, body, imgSrc, order }) => {
+    const [switchElements, setSwitchElements] = useState(false);
+
+    // switch places of image & text in subsections when Width >= 950px
+    const switchOnResize = () => {
+        let isLandscape = false;
+
+        if (order === 2 && window.innerWidth >= 950) {
+            isLandscape = true;
+        }
+
+        setSwitchElements(isLandscape);
     }
+
+    useEffect(() => {
+        switchOnResize()
+        window.addEventListener('resize', switchOnResize)
+
+        return () => {
+            window.removeEventListener('resize', switchOnResize)
+        }
+    });
 
     return (
         <section>
             <div className='subsectionWrapper'>
-                <div className='subsectionContent'>
-                    <h2>{header}</h2>
-                    <h4>{body}</h4>
-                </div>
-
-                <div>
-                    <img src={imgSrc} alt='Placeholder' style={imgStyle}/>
-                </div>
+                {switchElements ?
+                    <>
+                        <div>
+                            <img src={imgSrc} alt='Placeholder' />
+                        </div>
+                        <div className='subsectionContent'>
+                            <h2>{header}</h2>
+                            <h4>{body}</h4>
+                        </div>
+                    </>
+                    :
+                    <>
+                        <div className='subsectionContent'>
+                            <h2>{header}</h2>
+                            <h4>{body}</h4>
+                        </div>
+                        <div>
+                            <img src={imgSrc} alt='Placeholder' />
+                        </div>
+                    </>
+                }
             </div>
         </section>
     )
