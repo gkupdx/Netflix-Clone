c249a4459f1ca4142c77c0b9ed1155f5478d14ee
Grady Ku <gkupdx@gmail.com>
2022-03-22 11:59:34 -0700

    Import & render new component; Import shared icons and prop-drill into relevant components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 //// App.js
 
+import { useState } from 'react';
+
 import Homepage from './components/Homepage';
 import Registration from './components/Registration';
 import RegForm from './components/RegForm';
@@ -9,13 +11,15 @@ import Payment from './components/Payment';
 import CreditOption from './components/CreditOption';
 import PaypalOption from './components/PaypalOption';
 import GiftOption from './components/GiftOption';
-
+import EditPlan from './components/EditPlan';
 import Loginpage from './components/Loginpage';
 import FooterAlt from './components/FooterAlt';
 
-import { useState } from 'react';
 import { IoGlobe } from 'react-icons/io5';
+import { IoCheckmarkOutline } from 'react-icons/io5';
+import { IoIosCheckmarkCircleOutline } from 'react-icons/io';
 import { AiFillCaretDown } from 'react-icons/ai';
+import { VscChevronRight } from 'react-icons/vsc';
 
 import logoImgSrc from './assets/netflix_text_logo.png'
 
@@ -41,6 +45,18 @@ function App() {
   // Caret down icon styling 
   const caretIconStyle = {
     color: "#000"
+  }
+
+  // SVG styling
+  const svgStyle = {
+    width: "60px",
+    height: "25px"
+  }
+
+  // Discover card PNG styling 
+  const pngStyle = {
+    width: "40px",
+    height: "25px"
   }
 
   return (
@@ -73,7 +89,7 @@ function App() {
         {/* STEP 2 of 3 REGISTRATION */}
         <Route path='/signup' element={
           <>
-            <SignUp logo={logoImgSrc} />
+            <SignUp logo={logoImgSrc} checkMarkCircle={<IoIosCheckmarkCircleOutline style={{ color: "#e50914", fontSize: "3.5rem" }}/>} checkMark={<IoCheckmarkOutline style={{ color: "#e50914", fontSize: "2rem" }}/>}/>
             <FooterAlt toggleLanguages={toggleLanguages} toggle={toggle} globeIcon={<IoGlobe style={globeIconStyle} />} caretIcon={<AiFillCaretDown style={caretIconStyle} />} theme={footerTheme} />
           </>
         } />
@@ -81,7 +97,7 @@ function App() {
         {/* STEP 2 continued... */}
         <Route path='/signup/planform' element={
           <>
-            <PlanForm logo={logoImgSrc} />
+            <PlanForm logo={logoImgSrc} checkMarkLg={<IoCheckmarkOutline style={{ color: "#e50914", fontSize: "2rem" }}/>} checkMarkSm={<IoCheckmarkOutline style={{ fontSize: "2rem" }}/>}/>
             <FooterAlt toggleLanguages={toggleLanguages} toggle={toggle} globeIcon={<IoGlobe style={globeIconStyle} />} caretIcon={<AiFillCaretDown style={caretIconStyle} />} theme={footerTheme} />
           </>
         } />
@@ -89,7 +105,7 @@ function App() {
         {/* STEP 3 of 3 REGISTRATION */}
         <Route path='/signup/payment' element={
           <>
-            <Payment logo={logoImgSrc} />
+            <Payment logo={logoImgSrc} chevronRight={<VscChevronRight style={{ color: "lightgrey", fontSize: "2rem" }}/>} svgStyle={svgStyle} pngStyle={pngStyle}/>
             <FooterAlt toggleLanguages={toggleLanguages} toggle={toggle} globeIcon={<IoGlobe style={globeIconStyle} />} caretIcon={<AiFillCaretDown style={caretIconStyle} />} theme={footerTheme} />
           </>
         } />
@@ -97,7 +113,7 @@ function App() {
         {/* STEP 3 continued... Credit */}
         <Route path='/signup/creditoption' element={
           <>
-            <CreditOption logo={logoImgSrc} />
+            <CreditOption logo={logoImgSrc} svgStyle={svgStyle} pngStyle={pngStyle}/>
             <FooterAlt toggleLanguages={toggleLanguages} toggle={toggle} globeIcon={<IoGlobe style={globeIconStyle} />} caretIcon={<AiFillCaretDown style={caretIconStyle} />} theme={footerTheme} />
           </>
         } />
@@ -114,6 +130,14 @@ function App() {
         <Route path='/signup/giftoption' element={
           <>
             <GiftOption logo={logoImgSrc} />
+            <FooterAlt toggleLanguages={toggleLanguages} toggle={toggle} globeIcon={<IoGlobe style={globeIconStyle} />} caretIcon={<AiFillCaretDown style={caretIconStyle} />} theme={footerTheme} />
+          </>
+        }/>
+
+        {/* EDIT PLAN */}
+        <Route path='/signup/editplan' element={
+          <>
+            <EditPlan logo={logoImgSrc} />
             <FooterAlt toggleLanguages={toggleLanguages} toggle={toggle} globeIcon={<IoGlobe style={globeIconStyle} />} caretIcon={<AiFillCaretDown style={caretIconStyle} />} theme={footerTheme} />
           </>
         }/>
