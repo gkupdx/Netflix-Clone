93c96e0bf9dcc929531d57bdf097da97e197390c
Grady Ku <gkupdx@gmail.com>
2022-04-08 14:17:54 -0700

    Update CSS file imports

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './index_550.css';
 import './index_950.css';
-import './login.css';
-import './login_500.css';
-// import './login_950.css';
 import './registration.css';
 import './registration_500.css';
+import './login.css';
+import './login_500.css';
+import './login_745.css';
 import App from './App';
 
 // React Router
